afba590c75e55a5707e02a2ac0678b50787d278f
JavierlmC2K <alejavlopez@gmail.com>
2025-06-13 13:53:10 -0600

    sin comentario

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ function App() {
 
 export default App;*/
 
+//  Ramificación de JAVIERLM
+
 import "./App.css";
 import ProductList from "./components/ProductList";
 import CartSidebar from "./components/CartSidebar";
